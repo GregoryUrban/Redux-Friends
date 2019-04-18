122adc38f53a1fb69ea8302e234519637f9ca62f
GregoryUrban <turknben@gmail.com>
2019-04-18 10:06:15 -0400

    Added dependencies, store, provider, login

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
+import Login from "./components/Login"
 
 class App extends Component {
   render() {
     return (
+
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact path="/protected" component={GasPrices} />
       </div>
+    </Router>
+
     );
   }
 }
 
 export default App;
+
+
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
--- friends/src/index.js
+++ friends/src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from "redux"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import reducer from './reducers'
+import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+
+    })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk, logger)); // added from Swapi
+
+// const store = createStore(reducer, applyMiddleware(thunk)) // original
+const store = createStore(reducer,enhancer); // added from Swapi
+
+window.reduxStore = store
+const rootElement = document.getElementById("root")
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+)
+
+
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
